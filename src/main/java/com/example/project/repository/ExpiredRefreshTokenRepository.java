8034f9a4d15e870ac5cf120c20fbb73d76d478ab
softwarerbfl <32202018@dankook.ac.kr>
2022-03-01 19:36:09 +0900

    token 삭제

--- src/main/java/com/example/project/repository/ExpiredRefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.entity.ExpiredRefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpiredRefreshTokenRepository extends JpaRepository<ExpiredRefreshToken,Long> {
-    boolean existsByToken(String token);
-}
